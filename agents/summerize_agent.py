56e873ab462a4e1e027db3bb7fa4336d12ed0967
Roshani singh <sheenyrajput01@gmail.com>
2025-07-15 21:18:27 +0530

    summerize paper_text feature added

--- agents/summerize_agent.py
+++ agents/summerize_agent.py
@@ -1,3 +1,14 @@
+from transformers import pipeline
 
+summerizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
 
+def summarize_text(text, max_chunk_len = 1000):
+    chunks = [text[i:i+max_chunk_len] for i in range (0, len(text), max_chunk_len)]
+    summaries= []
 
+    # iterate through each chunk and summarize
+    for chunk in chunks:
+        chunk_summary = summerizer(chunk,max_chunk_len,min_length= 50, do_sample=False)[0]['summary_text']
+        summaries.append(chunk_summary)
+
+        return " ".join(summaries)
