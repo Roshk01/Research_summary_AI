21d98be6ea9115dcb8264cf47fc434bf3558e2e3
Roshani singh <sheenyrajput01@gmail.com>
2025-07-15 21:51:29 +0530

    synthesis summarization is added and tested

--- agents/synthesis_agent.py
+++ agents/synthesis_agent.py
@@ -2,14 +2,17 @@ from transformers import pipeline
 
 summerizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
 
-def summarize_text(summarize_by_topic, max_chunk_len=1000):
+def synthesise_summary(summarize_by_topic, max_chunk_len=1000):
     synthesized_output = []
 
     for topic, text in summarize_by_topic.items():
         combined_text = " ".join(text)
         chunks = [combined_text[i:i + max_chunk_len] for i in range(0,len(combined_text), max_chunk_len)]
+
+        topic_synthesis = []
         for chunk in chunks:
             chunk_summary = summerizer(chunk, max_length=150, min_length=40, do_sample=False)[0]['summary_text']
-            synthesized_output.append(f"Topic: {topic}\nSummary: {chunk_summary}")
+            topic_synthesis.append(chunk_summary)
 
-            return synthesized_output
+        synthesized_output[topic] = " ".join(topic_synthesis)
+    return synthesized_output
